f50b2e183ebe1d2a9ab95e9f3118541d976d5682
Dan Manning <dmanning23@hotmail.com>
2016-11-03 18:12:23 -0500

    fixed namespace

--- HighScoreBuddy/HighScoreBuddy.SharedProject/HighScoreTableComponent.cs
+++ HighScoreBuddy/HighScoreBuddy.SharedProject/HighScoreTableComponent.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace HighScoreBuddy.SharedProject
+namespace HighScoreBuddy
 {
 	public class HighScoreTableComponent : DrawableGameComponent, IHighScoreTable
 	{
